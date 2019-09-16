0accb375728c83a1fe1ee130b9ce5ae3b5632450
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-16 13:47:11 -0500

    Router Dinamic

---
+++ pages/albums/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hola mundo album id</h1>
+</template>
+<script>
+import router from "vue-router";
+export default {
+  name: "AlbumIndvPage",
+  created() {
+   let albumId = this.$route.params.id;
+   console.log(albumId)
+  }
+};
+</script>
