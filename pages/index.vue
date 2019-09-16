e65b0d42d01d7fe3e2a0342274b0e1dcef961c29
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-16 10:41:06 -0500

    axios y Ajax

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
-  <h1>Hola mundo</h1>
+  <h1>{{ mensaje }}</h1>
 </template>
 <script>
+import axios from "axios";
+import env from "../config/env";
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+  data() {
+    return {
+      mensaje: ""
+    };
+  },
+  created() {
+    axios.get(`${env.endpoint}/albums`).then(response => {
+      this.mensaje = response.status;
+    });
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
