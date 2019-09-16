c6c74c09b370df2fc0b09bead85af881ebc87b40
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-16 11:12:11 -0500

    use components

---
+++ components/AlbumCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{album.title}}</h3>
+    <a href="#">Ver album</a>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'AlbumCard',
+  props: {
+    album: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="">
     <h1>Albums de la pagina</h1>
-    <div v-for="album in albums" :key="album.title">
-      <p>{{ album.title }}</p>
-    </div>
+    <AlbumCard :album="album" v-for="album in albums" :key="album.id" />
   </div>
 </template>
 <script>
+
 import axios from "axios";
 import env from "../config/env";
+import AlbumCard from "../components/AlbumCard"
 export default {
   name: "IndexPage",
+  components: {
+    AlbumCard
+  },
   data() {
     return {
       albums: []
